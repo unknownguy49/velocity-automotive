f5897cb614dacf9eb9e2447bd595537d27be1656
Unknown Guy <91245369+unknownguy49@users.noreply.github.com>
2025-07-04 20:20:50 +0530

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,25 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'Velocity Automotive',
-  description: 'Frontend Battle',
-  generator: 'Dibyadyuti Dutta',
-}
+  title: "Velocity Automotive",
+  description: "Frontend Battle",
+  generator: "Dibyadyuti Dutta",
+  icons: {
+    icon: "/images/va.png",
+    shortcut: "/images/va.png",
+    apple: "/images/va.png",
+  },
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
